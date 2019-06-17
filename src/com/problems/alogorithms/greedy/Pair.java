a8fa2cfb79aa7608b0455b0ddeda77794756ef70
Ritesh Kasat <ritkasat@amazon.com>
2019-06-16 18:22:13 -0700

    Activity Selection Problem
    This problem uses greedy algorithm to schedule maximum number of jobs

---
+++ src/com/problems/alogorithms/greedy/Pair.java
@@ -0,0 +1,42 @@
+package com.problems.alogorithms.greedy;
+
+public class Pair implements Comparable<Pair> {
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    private long startTime;
+    private long endTime;
+    private int key;
+
+    public int getKey() {
+        return key;
+    }
+
+
+    public Pair(long startTime, long endTime, int key) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.key = key;
+    }
+
+
+    @Override
+    public int compareTo(Pair p) {
+        Long value = this.endTime - p.endTime;
+        return value.intValue();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}
---
+++ src/com/problems/alogorithms/greedy/activityselection/GFG.java
@@ -0,0 +1,65 @@
+package com.problems.alogorithms.greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class GFG {
+    private List<Pair> times;
+
+    public GFG(List<Pair> times) {
+        this.times = times;
+    }
+
+
+    public List<Integer> getMaxCount() {
+        Collections.sort(times);
+        List<Integer> list = new ArrayList<>();
+        list.add(times.get(0).getKey());
+
+        int size = times.size();
+
+        long c = times.get(0).getEndTime();
+
+        for (int i = 0; i < size - 1; i++) {
+
+            Pair next = times.get(i + 1);
+
+            if (c <= next.getStartTime()) {
+                c = next.getEndTime();
+                list.add(next.getKey());
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfTests = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < numberOfTests; i++) {
+            List<Pair> pairs  = readInput(sc);
+            GFG activitySelection = new GFG(pairs);
+
+            List<Integer> result = activitySelection.getMaxCount();
+            for (Integer item : result) {
+                System.out.print(item + " ");
+            }
+        }
+
+    }
+
+    public static List<Pair> readInput(Scanner sc) {
+        int count = Integer.parseInt(sc.nextLine());
+        List<String> l1 = Arrays.asList(sc.nextLine().split(" "));
+        List<String> l2 = Arrays.asList(sc.nextLine().split(" "));
+
+        List<Pair> pairs = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            pairs.add(new Pair(Long.parseLong(l1.get(i)), Long.parseLong(l2.get(i)), i + 1));
+        }
+        return pairs;
+    }
+}
